0e485eb2c8add38841a168d853db915665a81e01
cisco <sovisrushain@gmail.com>
2022-08-14 22:02:42 +0530

    initial commit

---
+++ src/main/java/com/cisco/Excp/Demo/ExcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.Excp.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cisco/Excp/Demo/ExcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.Excp.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
